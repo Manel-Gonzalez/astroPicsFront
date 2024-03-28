50cf0845cc050a3b001ebb69cb8e11580caae8c0
Manel-Gonzalez <manel.gonzalez.rico@gmail.com>
2024-03-28 17:57:56 +0100

    netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import {defineConfig} from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind()],
-  adapter: node({
-    mode: "middleware",
+  adapter: netlify({
+    edgeMiddleware: true,
   }),
 });
